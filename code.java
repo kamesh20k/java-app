d9bf01a19fe9ea59d459a77d1a766ebdc82338fa
“admin” <admin@gmail.com>
2022-01-04 06:53:42 +0000

    2

--- code.java
+++ code.java
@@ -4,10 +4,10 @@ public class RandomNumberExample1
 public static void main(String args[])   
 {   
 // Generating random numbers  
-System.out.println("1st Random Number: " + Math.random());   
-System.out.println("2nd Random Number: " + Math.random());  
-System.out.println("3rd Random Number: " + Math.random());   
-System.out.println("4th Random Number: " + Math.random());   
+System.out.println("1st Random Numbers: " + Math.random());   
+System.out.println("2nd Random Numbers: " + Math.random());  
+System.out.println("3rd Random Numbers: " + Math.random());   
+System.out.println("4th Random Numbers: " + Math.random());   
 }   
 }  
 
